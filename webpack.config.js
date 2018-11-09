5d8a5ca368381551f5aad4ea2889ffd1e9ee5bf6
Daniel <dds2130@rit.edu>
2018-11-08 22:54:34 -0500

    updated webpack for proper livereload and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,69 @@
 const ChromeExtensionReloader  = require('webpack-chrome-extension-reloader');
+// https://github.com/rubenspgcavalcante/webpack-chrome-extension-reloader
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// https://github.com/webpack-contrib/extract-text-webpack-plugin
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+// https://github.com/webpack-contrib/copy-webpack-plugin
 const path = require('path');
 
 var config = {
 	entry: {
-	    main: './plugin/js/main.js',
-	    background: './plugin/background.js'
+    "content-script": './plugin/js/main.js',
+    "background": './plugin/background.js'
+	},
+
+	// plugins: [
+ //    new ChromeExtensionReloader({
+ //      entries: { 
+ //        "content-script": ['main'],
+ //        background: 'background'
+ //      }
+ //    })
+	// ],
+
+	output: {
+	publicPath: ".",
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/'),
+    libraryTarget: "umd"
 	},
 
 	plugins: [
-	    new ChromeExtensionReloader({
-	      entries: { 
-	        contentScript: ['main'],
-	        background: 'background'
-	      }
-	    })
-	],
+		process.env.NODE_ENV === "development"? new ChromeExtensionReloader() : null,
 
-	output: {
-	   filename: '[name].js',
-	   path: path.join(__dirname, 'dist/')
+		// new ExtractTextPlugin({ filename: "css/[name].css" }),
+
+		new CopyWebpackPlugin([
+			{ from: "./plugin/manifest.json", flatten: true }, // manifest
+			{ from: "./plugin/css", to: 'css', toType: 'dir'}, // css
+			{ from: "./plugin/lib", to: 'lib', toType: 'dir'}, // lib
+			{ from: "./plugin/html",  to: 'html', toType: 'dir'}, // html
+			{ from: "./plugin/icons",  to: 'icons', toType: 'dir'} // icons
+		])
+
+	].filter(plugin => !!plugin),
+
+	module: {
+	  rules: [
+	  	{
+	    	test: /\.js?$/,
+	    	exclude: /node_modules/,
+  	  	use: {
+  	  		loader: "babel-loader",
+      		options: {
+        		presets: [require("babel-preset-es2015")]
+	      	}
+	    	}
+	  	}, 
+	  	{
+      	test: /\.css$/,
+      	exclude: /node_modules/,
+      	use: ExtractTextPlugin.extract({
+       		fallback: "style-loader",
+        	use: "css-loader",
+      	})
+      }
+	  ]
 	}
 }
 
